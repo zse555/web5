76d787747b8c9f07eb00cc6fa8b163033a954866
zse <zse555@qq.com>
2016-11-21 13:44:53 +0800

    Signed-off-by: zse <zse555@qq.com>

--- src/com/zse/Test.java
+++ src/com/zse/Test.java
@@ -1,5 +1,19 @@
 package com.zse;
 
 public class Test {
-
+   private int sid;
+   private String name;
+public int getSid() {
+	return sid;
+}
+public void setSid(int sid) {
+	this.sid = sid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+   
 }
